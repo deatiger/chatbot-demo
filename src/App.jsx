f781a26a4c4966e63cf7ff2ec4e73f8f9a7426ee
deatiger <deatiger1492@gmail.com>
2020-04-30 03:13:31 +0900

    Modified class components to functional components with React Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,54 @@
-import React from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 import './assets/styles/style.css'
 import {db} from './firebase/index'
 import {AnswersList, Chats, Loading} from './components/index'
 import {FormDialog} from "./components/Forms/index";
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            answers: [],          // 回答コンポーネントに表示するデータ
-            chats: [],            // チャットコンポーネントに表示するデータ
-            currentId: "init",    // 現在の質問ID
-            dataset: {},          // 質問と回答のデータセット
-            open: false           // 問い合わせフォーム用モーダルの開閉を管理
-        };
+const App = () => {
+    const [answers, setAnswers] = useState([]);            // 回答コンポーネントに表示するデータ
+    const [chats, setChats] = useState([]);                // チャットコンポーネントに表示するデータ
+    const [currentId, setCurrentId] = useState('init');    // 現在の質問ID
+    const [dataset, setDataset] = useState({});            // 質問と回答のデータセット
+    const [open, setOpen] = useState(false);               // 問い合わせフォーム用モーダルの開閉を管理
 
-        this.handleClose = this.handleClose.bind(this)
-        this.selectAnswer = this.selectAnswer.bind(this)
-    }
+    // 問い合わせフォーム用モーダルを開くCallback関数
+    const handleOpen = useCallback(() => {
+        setOpen(true)
+    },[setOpen]);
+
+    // 問い合わせフォーム用モーダルを閉じるCallback関数
+    const handleClose = useCallback(() => {
+        setOpen(false)
+    },[setOpen]);
+
+    // 新しいチャットを追加するCallback関数
+    const addChats = useCallback((chat) => {
+        setChats(prevChats => {
+            return [...prevChats, chat]
+        })
+    },[setChats]);
 
     // 次の質問をチャットエリアに表示する関数
-    displayNextQuestion = (nextQuestionId) => {
-        // 現在のチャット一覧を取得
-        const chats = this.state.chats;
-
-        // 次の質問をchatsに追加
-        chats.push({
-            text: this.state.dataset[nextQuestionId].question,
+    const displayNextQuestion = (nextQuestionId, nextDataset) => {
+        // 選択された回答と次の質問をチャットに追加
+        addChats({
+            text: nextDataset.question,
             type: 'question'
         });
 
-        this.setState({
-            answers: this.state.dataset[nextQuestionId].answers,    // 次の回答一覧
-            chats: chats,                                           // 選択された回答と次の質問をチャットに追加
-            currentId: nextQuestionId,                              // 現在の質問ID
-        })
-    }
+        // 次の回答一覧をセット
+        setAnswers(nextDataset.answers)
 
-    // 問い合わせフォーム用モーダルを開く関数
-    handleOpen = () => {
-        this.setState({open: true})
-    };
-    // 問い合わせフォーム用モーダルを閉じる関数
-    handleClose = () => {
-        this.setState({open: false})
-    };
-
-    initDataset = (dataset) => {
-        this.setState({dataset: dataset});
+        // 現在の質問IDをセット
+        setCurrentId(nextQuestionId)
     }
 
     // 回答が選択された時に呼ばれる関数
-    selectAnswer = (selectedAnswer, nextQuestionId) => {
+    const selectAnswer = (selectedAnswer, nextQuestionId) => {
         switch (true) {
-            // コンポーネントの初期化時
-            case (nextQuestionId === 'init'):
-                setTimeout(() => this.displayNextQuestion(nextQuestionId), 500);
-                break;
-
             // お問い合わせが選択された場合
             case (nextQuestionId === 'contact'):
-                this.handleOpen();
+                handleOpen();
                 break;
 
             // リンクが選択された時
@@ -74,63 +62,59 @@ class App extends React.Component {
             // 選択された回答をchatsに追加
             default:
                 // 現在のチャット一覧を取得
-                const chats = this.state.chats;
-
-                chats.push({
+                addChats({
                     text: selectedAnswer,
                     type: 'answer'
-                });
-                this.setState({
-                    chats: chats,
-                });
-                setTimeout(() => this.displayNextQuestion(nextQuestionId), 750)
+                })
+
+                setTimeout(() => displayNextQuestion(nextQuestionId, dataset[nextQuestionId]), 750)
                 break;
         }
     };
 
     // 最初の質問をチャットエリアに表示する
-    componentDidMount() {
+    useEffect(() => {
         (async() => {
-            const dataset = this.state.dataset;
+            const initDataset = {};
 
             // Fetch questions dataset from Firestore
             await db.collection('questions').get().then(snapshots => {
                 snapshots.forEach(doc => {
-                    dataset[doc.id] = doc.data()
+                    initDataset[doc.id] = doc.data()
                 })
             });
 
-            this.initDataset(dataset)
-            const initAnswer = "";
-            this.selectAnswer(initAnswer, this.state.currentId)
+            // Firestoreから取得したデータセットを反映
+            setDataset(initDataset);
+
+            // 最初の質問を表示
+            displayNextQuestion(currentId, initDataset[currentId])
         })();
-    }
+    }, []);
 
     // 最新のチャットが見えるように、スクロール位置の頂点をスクロール領域の最下部に設定する
-    componentDidUpdate(prevProps, prevState, snapshot) {
+    useEffect(() => {
         const scrollArea = document.getElementById('scroll-area');
         if (scrollArea) {
             scrollArea.scrollTop = scrollArea.scrollHeight;
         }
-    }
+    });
 
-    render() {
-        return (
-            <section className="c-section">
-                <div className="c-box">
-                    {(Object.keys(this.state.dataset).length === 0) ? (
-                        <Loading />
-                    ) : (
-                        <>
-                            <Chats chats={this.state.chats} />
-                            <AnswersList answers={this.state.answers} select={this.selectAnswer}/>
-                        </>
-                    )}
-                    <FormDialog open={this.state.open} handleOpen={this.handleOpen} handleClose={this.handleClose}/>
-                </div>
-            </section>
-        )
-    }
+    return (
+        <section className="c-section">
+            <div className="c-box">
+                {(Object.keys(dataset).length === 0) ? (
+                    <Loading />
+                ) : (
+                    <>
+                        <Chats chats={chats} />
+                        <AnswersList answers={answers} select={selectAnswer}/>
+                    </>
+                )}
+                <FormDialog open={open} handleOpen={handleOpen} handleClose={handleClose}/>
+            </div>
+        </section>
+    )
 }
 
 export default App;
--- src/components/Forms/FormDialog.jsx
+++ src/components/Forms/FormDialog.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -7,39 +7,30 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import {TextInput} from "./index";
 import {WEBHOOK_URL} from '../../webhookConfig'
 
-export default class FormDialog extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            description: "",
-            email: "",
-            name: ""
-        };
-
-        this.handleClose = this.props.handleClose.bind(this)
-        this.inputDescription = this.inputDescription.bind(this)
-        this.inputEmail = this.inputEmail.bind(this)
-        this.inputName = this.inputName.bind(this)
-        this.submitForm = this.submitForm.bind(this)
-    }
+const FormDialog = (props) => {
+    const [description, setDescription] = useState("");
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
 
     // Functions triggered by inputting text value
-    inputDescription = (event) => {
-        this.setState({ description: event.target.value })
-    };
-    inputEmail = (event) => {
-        this.setState({ email: event.target.value })
-    };
-    inputName = (event) => {
-        this.setState({ name: event.target.value })
-    };
+    const inputDescription = useCallback((event) => {
+        setDescription(event.target.value)
+    },[]);
 
-    validateEmailFormat = (email) => {
+    const inputEmail = useCallback((event) => {
+        setEmail(event.target.value)
+    },[]);
+
+    const inputName = useCallback((event) => {
+        setName(event.target.value)
+    },[]);
+
+    const validateEmailFormat = (email) => {
         const regex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
         return regex.test(email)
     }
 
-    validateRequiredInput = (...args) => {
+    const validateRequiredInput = (...args) => {
         let isBlank = false;
         for (let i = 0; i < args.length; i=(i+1)|0) {
             if (args[i] === "") {
@@ -50,13 +41,9 @@ export default class FormDialog extends React.Component {
     };
 
     // Slackに問い合わせがあったことを通知する
-    submitForm = () => {
-        const name = this.state.name;
-        const email = this.state.email;
-        const description = this.state.description;
-
-        const isBlank = this.validateRequiredInput(name, email, description)
-        const isValidEmail = this.validateEmailFormat(email)
+    const submitForm = () => {
+        const isBlank = validateRequiredInput(name, email, description)
+        const isValidEmail = validateEmailFormat(email)
 
         if (isBlank) {
             alert('必須入力欄が空白です。')
@@ -78,39 +65,42 @@ export default class FormDialog extends React.Component {
                 body: JSON.stringify(payload)
             }).then(() => {
                 alert('送信が完了しました。追ってご連絡いたします🙌');
-                return this.handleClose()
+                setDescription("")
+                setEmail("")
+                setName("")
+                return props.handleClose()
             })
         }
     };
 
-    render() {
-        return (
-            <Dialog open={this.props.open} onClose={this.handleClose}>
-                <DialogTitle>お問い合わせフォーム</DialogTitle>
-                <DialogContent>
-                    <TextInput
-                        label={"名前(必須)"} multiline={false} rows={1}
-                        value={this.state.name} type={"text"} onChange={this.inputName}
-                    />
-                    <TextInput
-                        label={"メールアドレス(必須)"} multiline={false} rows={1}
-                        value={this.state.email} type={"email"} onChange={this.inputEmail}
-                    />
-                    <TextInput
-                        label={"お問い合わせ内容(必須)"} multiline={true} rows={5}
-                        value={this.state.description} type={"text"} onChange={this.inputDescription}
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={this.handleClose} color="primary">
-                        キャンセル
-                    </Button>
-                    <Button onClick={this.submitForm} color="primary">
-                        送信する
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        );
-    }
+    return (
+        <Dialog open={props.open} onClose={props.handleClose}>
+            <DialogTitle>お問い合わせフォーム</DialogTitle>
+            <DialogContent>
+                <TextInput
+                    label={"名前(必須)"} multiline={false} rows={1}
+                    value={name} type={"text"} onChange={inputName}
+                />
+                <TextInput
+                    label={"メールアドレス(必須)"} multiline={false} rows={1}
+                    value={email} type={"email"} onChange={inputEmail}
+                />
+                <TextInput
+                    label={"お問い合わせ内容(必須)"} multiline={true} rows={5}
+                    value={description} type={"text"} onChange={inputDescription}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={props.handleClose} color="primary">
+                    キャンセル
+                </Button>
+                <Button onClick={submitForm} color="primary">
+                    送信する
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+
 }
 
+export default FormDialog
